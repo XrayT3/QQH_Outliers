57e05e42397c1273628b0d1c8a3ccd435a414f46
XrayT3 <sha0006@yandex.ru>
2024-11-09 11:27:16 +0100

    Fix bugs, set hyperparameters

--- src/model_xgboost.py
+++ src/model_xgboost.py
@@ -1,9 +1,14 @@
 import numpy as np
 import pandas as pd
-from math import log
+from math import log10
 
 from xgboost import XGBClassifier
-from xgboost.dask import predict
+
+
+def get_optimal_fractions(probabilities: np.ndarray, odds: pd.Series) -> np.ndarray:
+    odds = odds.to_numpy()
+    q = 1 - probabilities
+    return probabilities - (q / odds)
 
 
 class Model:
@@ -41,10 +46,12 @@ class Model:
         }
 
     def __init__(self):
-        self.lr = 0.06
-        self.gamma = 0.5
+        self.lr = 0.26
+        self.gamma = 0.5 # or 0.3
         self.teams_n = 2
-        self.features_n = 8 * self.teams_n + 1
+        self.cnf_threshold = 0.2
+        self.minimal_games = 39 * 4
+        self.features_n = 2 * self.teams_n
         self.has_model = False
         self.team_stats = {}
         self.team_pi_rating = {}
@@ -54,8 +61,6 @@ class Model:
         self.season = -1
         # TODO: set up hyperparameters
         self.model = XGBClassifier(max_depth=4, subsample=0.8, min_child_weight=5, colsample_bytree=0.25, seed=24)
-        # TODO: do we need scaler?
-        # self.scaler = sklearn.preprocessing.StandardScaler(with_mean=False)
 
     def __store_inc(self, games: pd.DataFrame, players: pd.DataFrame):
         if self.games.empty: self.games = games
@@ -66,7 +71,7 @@ class Model:
 
     def __is_enough_data(self):
         # 3 seasons is enough data
-        return len(self.games['Season'].unique()) == 3
+        return len(self.games['Season'].unique()) >= 3
 
     def __is_new_season(self, opps):
         curr_season = opps['Season'].iloc[0]
@@ -97,7 +102,7 @@ class Model:
 
     def __expected_goal_diff(self, team_id, key: str):
         rating = self.team_pi_rating[team_id][key]
-        b, c = 10, 3
+        b, c = 10, 2
         norm = 1 if rating >= 0 else -1
         return norm * (b ** (abs(rating) / c) - 1)
 
@@ -109,18 +114,18 @@ class Model:
 
         expected_diff = self.__expected_goal_diff(team_h, 'H RTG') - self.__expected_goal_diff(team_a, 'A RTG')
         real_diff = score_h - score_a
-        error = abs(expected_diff - real_diff)
-        ps = 3 * log(1 + error, 10)  # c = 3, b = 10
+        error = abs(real_diff - expected_diff)
+        ps = 2 * log10(1 + error)  # c = 2, b = 10
         ps_h = ps if expected_diff < real_diff else -ps
         ps_a = ps if expected_diff > real_diff else -ps
 
-        rah_hat = self.team_pi_rating[team_h]['H RTG'] + ps_h * self.lr
-        self.team_pi_rating[team_h]['A RTG'] += (rah_hat - self.team_pi_rating[team_h]['H RTG']) * self.gamma
-        self.team_pi_rating[team_h]['H RTG'] = rah_hat
+        old_team_h_h = self.team_pi_rating[team_h]['H RTG']
+        self.team_pi_rating[team_h]['H RTG'] += ps_h * self.lr
+        self.team_pi_rating[team_h]['A RTG'] += (self.team_pi_rating[team_h]['H RTG'] - old_team_h_h) * self.gamma
 
-        rba_hat = self.team_pi_rating[team_a]['A RTG'] + ps_a * self.lr
-        self.team_pi_rating[team_a]['H RTG'] += (rba_hat - self.team_pi_rating[team_a]['A RTG']) * self.gamma
-        self.team_pi_rating[team_a]['A RTG'] = rba_hat
+        old_team_a_a = self.team_pi_rating[team_a]['A RTG']
+        self.team_pi_rating[team_a]['A RTG'] += ps_a * self.lr
+        self.team_pi_rating[team_a]['H RTG'] += (self.team_pi_rating[team_a]['A RTG'] - old_team_a_a) * self.gamma
 
     def __delete_pi_ratings(self):
         for key in self.team_pi_rating.keys():
@@ -183,31 +188,31 @@ class Model:
     def get_features(self, team_h: int, team_a: int) -> np.ndarray:
         x_features = np.zeros(self.features_n)
         # home team's features
-        x_features[0] = self.team_historical_strength[team_h]['H WIN PCT']
-        x_features[1] = self.team_historical_strength[team_h]['A WIN PCT']
-        x_features[2] = self.team_historical_strength[team_h]['H GS AVG']
-        x_features[3] = self.team_historical_strength[team_h]['A GS AVG']
-        x_features[4] = self.team_historical_strength[team_h]['H GC AVG']
-        x_features[5] = self.team_historical_strength[team_h]['A GC AVG']
-        x_features[6] = self.team_pi_rating[team_h]['H RTG']
-        x_features[7] = self.team_pi_rating[team_h]['A RTG']
+        x_features[0] = self.team_pi_rating[team_h]['H RTG']
+        x_features[1] = self.team_pi_rating[team_h]['A RTG']
+        # x_features[2] = self.team_historical_strength[team_h]['H WIN PCT']
+        # x_features[3] = self.team_historical_strength[team_h]['A WIN PCT']
+        # x_features[2] = self.team_historical_strength[team_h]['H GS AVG']
+        # x_features[3] = self.team_historical_strength[team_h]['A GS AVG']
+        # x_features[4] = self.team_historical_strength[team_h]['H GC AVG']
+        # x_features[5] = self.team_historical_strength[team_h]['A GC AVG']
         # x_features[6] = self.team_historical_strength[team_h]['H GS STD']
         # x_features[7] = self.team_historical_strength[team_h]['A GS STD']
         # x_features[8] = self.team_historical_strength[team_h]['H GC STD']
         # x_features[9] = self.team_historical_strength[team_h]['A GC STD']
 
         # away team's features
-        x_features[8] = self.team_historical_strength[team_a]['H WIN PCT']
-        x_features[9] = self.team_historical_strength[team_a]['A WIN PCT']
-        x_features[10] = self.team_historical_strength[team_a]['H GS AVG']
-        x_features[11] = self.team_historical_strength[team_a]['A GS AVG']
-        x_features[12] = self.team_historical_strength[team_a]['H GC AVG']
-        x_features[13] = self.team_historical_strength[team_a]['A GC AVG']
-        x_features[14] = self.team_pi_rating[team_a]['H RTG']
-        x_features[15] = self.team_pi_rating[team_a]['A RTG']
-
-        expected_diff = self.__expected_goal_diff(team_h, 'H RTG') - self.__expected_goal_diff(team_a, 'A RTG')
-        x_features[16] = expected_diff
+        x_features[2] = self.team_pi_rating[team_a]['H RTG']
+        x_features[3] = self.team_pi_rating[team_a]['A RTG']
+        # x_features[6] = self.team_historical_strength[team_a]['H WIN PCT']
+        # x_features[7] = self.team_historical_strength[team_a]['A WIN PCT']
+        # x_features[12] = self.team_historical_strength[team_a]['H GS AVG']
+        # x_features[13] = self.team_historical_strength[team_a]['A GS AVG']
+        # x_features[14] = self.team_historical_strength[team_a]['H GC AVG']
+        # x_features[15] = self.team_historical_strength[team_a]['A GC AVG']
+        #
+        # expected_diff = self.__expected_goal_diff(team_h, 'H RTG') - self.__expected_goal_diff(team_a, 'A RTG')
+        # x_features[4] = expected_diff
         # x_features[16] = self.team_historical_strength[team_a]['H GS STD']
         # x_features[17] = self.team_historical_strength[team_a]['A GS STD']
         # x_features[18] = self.team_historical_strength[team_a]['H GC STD']
@@ -230,9 +235,11 @@ class Model:
             match = row[1] # one row of dataframe
             team_h, team_a = match['HID'], match['AID']
 
-            # TODO: maybe improve this
             # if we have a new team in the current season we skip these games
             if team_a not in self.team_stats or team_h not in self.team_stats: continue
+            # if we have little information about team we skip it
+            if self.team_stats[team_a]['H GM CNT'] + self.team_stats[team_a]['A GM CNT'] < self.minimal_games: continue
+            if self.team_stats[team_h]['H GM CNT'] + self.team_stats[team_h]['A GM CNT'] < self.minimal_games: continue
 
             # get features
             x_train[i, :] = self.get_features(team_h, team_a)
@@ -262,11 +269,22 @@ class Model:
                 skipped.append(i)
                 continue
 
+            if self.team_stats[team_a]['H GM CNT'] + self.team_stats[team_a]['A GM CNT'] < self.minimal_games:
+                skipped.append(i)
+                continue
+            if self.team_stats[team_h]['H GM CNT'] + self.team_stats[team_h]['A GM CNT'] < self.minimal_games:
+                skipped.append(i)
+                continue
+
             x_data[i, :] = self.get_features(team_h, team_a)
 
         return x_data, skipped
 
     def place_bets(self, summary: pd.DataFrame, opps: pd.DataFrame, inc: tuple[pd.DataFrame, pd.DataFrame]):
+        bankroll = summary.iloc[0]['Bankroll']
+        min_bet = summary.iloc[0]['Min_bet']
+        n = len(opps)
+
         # store data
         self.__store_inc(inc[0], inc[1])
 
@@ -279,29 +297,32 @@ class Model:
 
         # retrain model every season
         if self.__is_new_season(opps) and self.__is_enough_data():
-            print('train')
             x_train, y_train = self.get_train_data()
             self.model.fit(x_train, y_train)
             self.has_model = True
 
         # make prediction
         predictions = np.argmin(opps[['OddsH', 'OddsA']], axis=1)
-        skipped = []
+        no_bet = [np.arange(n)]
 
         if self.has_model:
-            x, skipped = self.get_data(opps)
+            x, no_info = self.get_data(opps)
             predictions = self.model.predict(x)
+            probs = self.model.predict_proba(x)
+            # confidence threshold
+            confidence_threshold = np.where(np.all(probs < 0.5 + self.cnf_threshold, axis=1))[0]
+            no_bet = np.union1d(no_info, confidence_threshold).astype(int)
+            fractions = get_optimal_fractions(probs, opps[['OddsH', 'OddsA']])
 
         # chose bets
+
         # TODO: check if there is already my bet
 
+
         # place bets
-        min_bet = summary.iloc[0]["Min_bet"]
-        N = len(opps)
-        bets = np.zeros((N, 2))
-        bets[np.arange(N), predictions] = min_bet
-        bets[skipped, :] = 0
-        # bets[np.arange(N), np.random.choice([0, 1])] = min_bet
+        bets = np.zeros((n, 2))
+        bets[np.arange(n), predictions] = min_bet
+        bets[no_bet, :] = 0
         # bets[np.arange(N), np.argmin(opps[['OddsH', 'OddsA']], axis=1)] = min_bet
         bets = pd.DataFrame(data=bets, columns=["BetH", "BetA"], index=opps.index)
         return bets
